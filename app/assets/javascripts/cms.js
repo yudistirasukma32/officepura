e514a6bd87e3039b0b4c8f386c8bf3336e730921
Yudistira S.D <yudistira@mydevteam.com.au>
2025-02-26 11:56:20 +0700

    revert js

--- app/assets/javascripts/cms.js
+++ app/assets/javascripts/cms.js
@@ -3618,45 +3618,6 @@ $(document).ready(function() {
 		getGraphAnnualReportVehicle();
 	}
 
-	//calculate distance
-	$(document).on('click', '#calculateDistance', function(e) {
-		e.preventDefault();
-
-		alert('Melakukan kalkulasi jarak (km).');
-
-		var longitudeStart = $("#quotation_longitude_start").val();
-		var latitudeStart = $("#quotation_latitude_start").val();
-		var longitudeEnd = $("#quotation_longitude_end").val();
-		var latitudeEnd = $("#quotation_latitude_end").val();
-
-		var distanceKm = $("quotation_distance").val();
-
-		if (!longitudeStart || !latitudeStart || !longitudeEnd || !latitudeEnd) {
-
-			alert("Data Longitude dan Latitude peta harus terisi.");
-
-			e.preventDefault(); // Stop form submission
-
-		} else {
-
-			// Construct dynamic URL
-			var url = `/routelocations/checkDistance?latitude_start=${latitudeStart}&longitude_start=${longitudeStart}&latitude_end=${latitudeEnd}&longitude_end=${longitudeEnd}`;
-
-			$.ajax({
-				type: "GET",
-				url: url,
-				success: function(data) {
-					alert('Estimasi jarak: ' + data.distance_km + ' km');
-					console.log(data.distance_km + ' km');
-					$("#quotation_distance").val(data.distance_km).focus();
-				},
-				failure: function() {alert("Error. Mohon refresh browser Anda.");}
-			});
-
-		}
-
-	});
-
 	if ($('.vehicle_autocomplete').length > 0){
 		// $( ".vehicle_autocomplete" ).autocomplete({
   //     source: '/taxinvoices/generic-vehicles'});
