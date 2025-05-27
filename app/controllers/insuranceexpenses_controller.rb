118b3e28fb46a586efdc8e20750063be2bc39619
Yudistira S.D <yudistira@mydevteam.com.au>
2025-05-27 12:14:50 +0700

    update claimmemos index add inv, update insurance expenses index

--- app/controllers/insuranceexpenses_controller.rb
+++ app/controllers/insuranceexpenses_controller.rb
@@ -13,7 +13,7 @@ class InsuranceexpensesController < ApplicationController
     @user_role = 'Admin Keuangan, Admin Operasional, Admin Asuransi'
   end
 
-  def index
+  def index_old
 
     role = cek_roles @user_role
     if role
@@ -42,7 +42,46 @@ class InsuranceexpensesController < ApplicationController
 
   end
 
-  def paid
+  def index
+    role = cek_roles(@user_role)
+    unless role
+      redirect_to root_path
+      return
+    end
+
+    @where = "insuranceexpenses"
+    @startdate = params[:startdate].present? ? params[:startdate] : Date.today.strftime('%d-%m-%Y')
+    @enddate = params[:enddate].present? ? params[:enddate] : Date.today.strftime('%d-%m-%Y')
+
+    date_range = @startdate.to_date..@enddate.to_date
+
+    excluded_invoice_ids = (Insuranceexpense.where(deleted: false).select(:invoice_id).map(&:invoice_id) +
+                             Invoicereturn.where(deleted: false).select(:invoice_id).map(&:invoice_id)).flatten.uniq
+
+    base_insurance_scope = Invoice.joins(:event)
+                             .where(is_insurance: true,
+                                    date: date_range)
+                             .where('invoices.id not in (?)',excluded_invoice_ids)
+                             .where('events.tsi_total > money(0)')
+                             .where('events.is_insurance = ?', true)
+
+    @insuranceexpenses = base_insurance_scope.where(invoicetrain: false).order(:date, :event_id)
+    @insuranceexpenses_loading = base_insurance_scope.where(invoicetrain: true).where('invoice_id is null').order(:date, :event_id)
+    @insuranceexpenses_dooring = base_insurance_scope.where(invoicetrain: true).where('invoice_id is not null').order(:date, :event_id)
+    @insuranceexpenses_losing = base_insurance_scope.where(invoicetrain: false, 'events.losing' => true).order(:date, :event_id) # Using hash syntax for string key
+
+    @commodity_id = params[:commodity_id]
+    if @commodity_id.present?
+      @insuranceexpenses = @insuranceexpenses.joins(:event).where('events.commodity_id = ?', @commodity_id)
+      @insuranceexpenses_loading = @insuranceexpenses_loading.joins(:event).where('events.commodity_id = ?', @commodity_id)
+      @insuranceexpenses_dooring = @insuranceexpenses_dooring.joins(:event).where('events.commodity_id = ?', @commodity_id)
+      @insuranceexpenses_losing = @insuranceexpenses_losing.joins(:event).where('events.commodity_id = ?', @commodity_id)
+    end
+
+    respond_to :html
+  end
+
+  def paid_old
     @where = "insuranceexpenses-paid"
 
     @startdate = params[:startdate]
@@ -66,6 +105,40 @@ class InsuranceexpensesController < ApplicationController
 
     respond_to :html
   
+  end
+
+  def paid
+    @where = "insuranceexpenses-paid"
+    @startdate = params[:startdate].presence || Date.today.strftime('%d-%m-%Y')
+    @enddate = params[:enddate].presence || Date.today.strftime('%d-%m-%Y')
+    date_range = @startdate.to_date..@enddate.to_date
+
+    base_paid_scope = Insuranceexpense.joins(invoice: :event)
+                                     .where(insuranceexpenses: { date: date_range, deleted: false })
+                                     .order('insuranceexpenses.id')
+
+    @insuranceexpenses = base_paid_scope.where('invoices.invoicetrain = false')
+    @insuranceexpenses_loading = base_paid_scope.where('invoices.invoicetrain = true AND invoices.invoice_id IS NULL')
+    @insuranceexpenses_dooring = base_paid_scope.where('invoices.invoicetrain = true AND invoices.invoice_id IS NOT NULL')
+    @insuranceexpenses_losing = base_paid_scope.where('invoices.invoicetrain = false AND events.losing = true')
+
+    @insurancevendor_id = params[:insurancevendor_id]
+    if @insurancevendor_id.present?
+      @insuranceexpenses = @insuranceexpenses.where(insuranceexpenses: { insurancevendor_id: @insurancevendor_id })
+      @insuranceexpenses_loading = @insuranceexpenses.where(insuranceexpenses: { insurancevendor_id: @insurancevendor_id })
+      @insuranceexpenses_dooring = @insuranceexpenses.where(insuranceexpenses: { insurancevendor_id: @insurancevendor_id })
+      @insuranceexpenses_losing = @insuranceexpenses.where(insuranceexpenses: { insurancevendor_id: @insurancevendor_id })
+    end
+
+    @commodity_id = params[:commodity_id]
+    if @commodity_id.present?
+      @insuranceexpenses = @insuranceexpenses.where('events.commodity_id = ?', @commodity_id)
+      @insuranceexpenses_loading = @insuranceexpenses.where('events.commodity_id = ?', @commodity_id)
+      @insuranceexpenses_dooring = @insuranceexpenses.where('events.commodity_id = ?', @commodity_id)
+      @insuranceexpenses_losing = @insuranceexpenses.where('events.commodity_id = ?', @commodity_id)
+    end
+
+    respond_to :html
   end
 
   def new
