3d827c86be8fc97839d182a1f9ac291a69ade771
Yudistira S.D <yudistira@mydevteam.com.au>
2025-06-20 13:53:06 +0700

    update approval query

--- app/controllers/approvals_controller.rb
+++ app/controllers/approvals_controller.rb
@@ -18,15 +18,18 @@ class ApprovalsController < ApplicationController
 
       @new_customers = Customer.active.unverified
 
-      @claimmemos = Claimmemo.active.where('approved = false or approved_by_gm = false')
+      # @claimmemos = Claimmemo.active.where('approved = false or approved_by_gm = false')
+      @claimmemos = Claimmemo.active.where('approved_marketing = false')
 
       @confirmed_quotation = Quotationgroup.active.pluck(:id)
       @quotation = Quotation.active.where('quotationgroup_id in (?)', @confirmed_quotation).pluck(:id)
       @allowances = Allowance.active.where('driver_trip = money(0)').pluck(:route_id)
-      @new_routes = Route.active.where('quotation_id in (?) and id in (?)', @quotation, @allowances)
+      # @new_routes = Route.active.where('quotation_id in (?) and id in (?)', @quotation, @allowances)
+      @new_routes = Route.active
+        .where('quotation_id IN (?) AND id IN (?) AND created_at >= ?', @quotation, @allowances, 30.days.ago)
 
       @quotationgroups = Quotationgroup.active.where('date >= ?', Date.new(2025, 1, 1)).reorder('date DESC')
-      @quotationgroups = @quotationgroups.where('customer_approved = false')
+      @quotationgroups = @quotationgroups.where('confirmed = false')
       
       @draft_routes = Route.active.where('approved = false')
       respond_to :html
