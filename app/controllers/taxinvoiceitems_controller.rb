87cbc7a312cc9e9f91bf7b4a7a36cc3eda6cfd94
Yudistira S.D <yudistira@mydevteam.com.au>
2025-08-22 09:47:54 +0700

    remove pura query on sj

--- app/controllers/taxinvoiceitems_controller.rb
+++ app/controllers/taxinvoiceitems_controller.rb
@@ -38,7 +38,7 @@ class TaxinvoiceitemsController < ApplicationController
     #hide bkk bongkar
     @invoices = @invoices.where('invoice_id is null')
     #hide bkk kosongan
-    @cust_kosongan = Customer.where("name ~* '.*PURA.*' or name ~* '.*RDPI.*' or name ~* '.*RAJAWALI INTI.*'").pluck(:id)
+    @cust_kosongan = Customer.where("name ~* '.*RDPI.*' or name ~* '.*RAJAWALI INTI.*'").pluck(:id)
 
     @invoices = @invoices.where("customer_id NOT IN (?)", @cust_kosongan).order(:id)
 
